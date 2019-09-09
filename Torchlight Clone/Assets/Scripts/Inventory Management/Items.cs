192e7399d629730c241e832b70fa3020f4baa450
Cody Wilcox <cwilcox8@stumail.jccc.edu>
2019-09-09 17:56:49 -0500

    Started work on getting the items set up

--- Torchlight Clone/Assets/Scripts/Inventory Management/Items.cs
+++ Torchlight Clone/Assets/Scripts/Inventory Management/Items.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Items
 {
+    public int item;
+    public int tint;
 
+    public Items()
+    {
+        item = 0;
+        tint = 0;
+    }
+
+
+    public void EnterItems(int itement)
+    {
+        item = itement;
+
+        tint = Random.Range(1, 5);
+    }
 }
---
+++ Torchlight Clone/Assets/Scripts/Inventory Management/SetupInventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetupInventory : MonoBehaviour
+{
+    public List<Items> itemList;
+    public int randomNumber;
+    public Image[] Inventory;
+    public Image[] Equipment;
+
+    private void Awake()
+    {
+        itemList = new List<Items>();
+        //Temporary
+
+        foreach (Image item in Inventory)
+        {
+            Items newItem = new Items();
+            itemList.Add(newItem);
+        }
+
+        for (int i = 0; i <= itemList.Count; i++)
+        {
+            itemList[i].EnterItem(Random.Range(1, 6));
+            if(itemList[i].item == 1)
+            {
+                randomNumber = Random.Range(1, 5);
+                //Finish
+                if (randomNumber = 1)
+                {
+                    //Use Asset Database
+                    Inventory[i].Sprite = "brutal-helm";
+                }
+            }
+        }
+    }
+}
