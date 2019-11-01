d3e4fe9de62621335b446b60a1dee47a5087f33c
Cody Wilcox <>
2019-11-01 18:46:46 -0500

    Need to make Photon Account, definitely not a note

---
+++ Shaders/Assets/MouseHighlight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHighlight : MonoBehaviour
+{
+    public Renderer myRenderer;
+
+    private void OnMouseEnter()
+    {
+        myRenderer.material.SetFloat("_HighlightOn", 1);
+    }
+
+    private void OnMouseExit()
+    {
+        myRenderer.material.SetFloat("_HighlightOn", 0);
+    }
+}
