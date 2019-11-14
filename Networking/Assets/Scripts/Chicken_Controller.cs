b27629192a92b0c7e52919b58f5a8bf35ee22db2
Cody Wilcox <>
2019-11-13 19:30:51 -0600

    Not removing players from room correctly, but other than that it's fine

--- Networking/Assets/Scripts/Chicken_Controller.cs
+++ Networking/Assets/Scripts/Chicken_Controller.cs
@@ -2,54 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
+using Photon.Realtime;
 
-public class Chicken_Controller : MonoBehaviourPun
+public class Chicken_Controller : MonoBehaviourPunCallbacks, IPunObservable
 {
     Animator animator;
     [SerializeField] float moveSpeed = 8;
-
-    UnityEngine.UI.Text scoreBox;
 
     int score = 0;
     [SerializeField] float timeToScore = 2;
     float currentScoreTimer = 0;
 
+    float oneHalf = 1 / 2;
+    float scoreUpdate = 0;
+
+    bool isEating = false;
+    bool isWalking = false;
+
+    ScoreReport scoreReport;
+    //bool doTest = false;
+
     private void Start()
     {
-        animator = GetComponent<Animator>();
-        scoreBox = GameObject.Find("txtScore").GetComponent<UnityEngine.UI.Text>();
+        scoreReport = GameObject.Find("UIManager").GetComponent<ScoreReport>();
+        if (photonView.IsMine)
+        {
+            animator = GetComponent<Animator>();
+            scoreReport.Register("local");
+        }
+        else
+        {
+            scoreReport.Register(photonView.Owner.NickName);
+        }
     }
 
     private void Update()
     {
         if (!photonView.IsMine)
         {
+            scoreReport.ScoreUpdate(photonView.Owner.NickName, score);
             return;
         }
+        scoreReport.ScoreUpdate("local", score);
         Vector3 movement = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
 
         if (movement.sqrMagnitude == 0)
         {
+            isWalking = false;
             animator.SetBool("Walk", false);
 
             if(Input.GetKey(KeyCode.Space))
             {
+                isEating = true;
                 animator.SetBool("Eat", true);
                 DoScoring();
             }
             else
             {
+                isEating = false;
                 animator.SetBool("Eat", false);
             }
         }
         else
         {
+            isWalking = true;
+            isEating = false;
             animator.SetBool("Walk", true);
             animator.SetBool("Eat", false);
             transform.position += movement.normalized * (moveSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(0, Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg, 0);
         }
-        scoreBox.text = string.Format("Score: {0}", score);
+        //scoreBoxes["Mine"].text = string.Format("My Score: {0}", score);
     }
 
     void DoScoring()
@@ -70,4 +94,61 @@ public class Chicken_Controller : MonoBehaviourPun
             }
         }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+        Chicken_Controller ckOther = other.gameObject.GetComponent<Chicken_Controller>();
+        if (ckOther != null && isWalking)
+        {
+            if (ckOther.isEating)
+            {
+                PhotonView otherView = other.gameObject.GetComponent<PhotonView>();
+                otherView.RPC("ReduceScore", RpcTarget.Others, otherView.ViewID);
+               // Debug.Log("Sending an RPC Call");
+            }
+        }
+    }
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (doTest && other.gameObject.tag == "Player")
+    //    {
+    //        PhotonView otherView = other.gameObject.GetComponent<PhotonView>();
+    //        otherView.RPC("ReduceScore", RpcTarget.Others, otherView.ViewID);
+    //    }
+    //}
+
+    [PunRPC] public void ReduceScore(int ID)
+    {
+        if (ID == this.photonView.ViewID)
+        {
+            //Debug.Log("Getting a Call");
+            scoreUpdate = score * oneHalf;
+            score = (int)scoreUpdate;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(isEating);
+            stream.SendNext(score);
+        }
+        else
+        {
+            this.isEating = (bool)stream.ReceiveNext();
+            this.score = (int)stream.ReceiveNext();
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        scoreReport.UnRegister(otherPlayer.NickName);
+    }
+
+
 }
---
+++ Networking/Assets/Scripts/ScoreReport.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreReport : MonoBehaviour
+{
+    Dictionary<string, Text> scoreBoxes;
+    private string firstPerson;
+    private string secondPerson;
+    private string thirdPerson;
+    [SerializeField] List<GameObject> textRefs;
+    private void Start()
+    {
+        scoreBoxes = new Dictionary<string, Text>();
+        //if (photonView.IsMine)
+        //{
+        //    //animator = GetComponent<Animator>();
+
+        //}
+        //scoreBoxes.Add("local", GameObject.Find("My Panel").GetComponentInChildren<Text>());
+        foreach (GameObject item in textRefs)
+        {
+            item.SetActive(false);
+        }
+
+    }
+    public void ScoreUpdate(string ID, int score)
+    {
+        scoreBoxes[ID].text = ID + ": " + score;
+    }
+
+    public void Register(string ID)
+    {
+        if (ID == "local")
+        {
+            scoreBoxes.Add("local", textRefs[0].GetComponentInChildren<Text>());
+            textRefs[0].SetActive(true);
+        }
+
+        else
+        {
+            scoreBoxes.Add(ID, textRefs[scoreBoxes.Count].GetComponentInChildren<Text>());
+            textRefs[scoreBoxes.Count].SetActive(true);
+            if (firstPerson == null)
+            {
+                firstPerson = ID;
+            }
+            else if (secondPerson == null)
+            {
+                secondPerson = ID;
+            }
+            else if (thirdPerson == null)
+            {
+                thirdPerson = ID;
+            }
+        }
+    }
+    public void UnRegister(string ID)
+    {
+        if (ID == "local")
+        {
+
+        }
+
+        else
+        {
+            scoreBoxes.Remove(ID);
+            if (firstPerson != null && firstPerson == ID)
+            {
+                textRefs[1].SetActive(false);
+            }
+            else if (secondPerson != null && secondPerson == ID)
+            {
+                textRefs[2].SetActive(false);
+            }
+            else if (thirdPerson != null && thirdPerson == ID)
+            {
+                textRefs[3].SetActive(false);
+            }
+        }
+    }
+}
