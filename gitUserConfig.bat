0bdfc05c4c0a00069e524620981c90866724b7a8
Cody Wilcox <cdwilcox1999@gmail.com>
2019-12-06 15:27:27 -0600

    Worked on the gitUserConfig, just need to make more of them now

--- gitUserConfig.bat
+++ gitUserConfig.bat
@@ -1,10 +1,11 @@
 @ECHO OFF
 
 cd desktop
-cmd /Wait git clone https://github.com/Zeroclipse/Torchlight-Clone.git
+Start /w git clone https://github.com/Zeroclipse/Torchlight-Clone.git
 cd torchlight-clone
 git config --global user.name "Cody Wilcox"
 git config --global user.email "cdwilcox1999@gmail.com"
 
+@ECHO ON
+
 cmd /K
-PAUSE
