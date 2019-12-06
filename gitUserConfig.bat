2877b2418393720f86c6bb21122d195ec9f62c4b
Cody Wilcox <cdwilcox1999@gmail.com>
2019-12-06 15:11:51 -0600

    Worked on the gituserconfig for later

--- gitUserConfig.bat
+++ gitUserConfig.bat
@@ -1,6 +1,10 @@
 @ECHO OFF
 
+cd desktop
+cmd /Wait git clone https://github.com/Zeroclipse/Torchlight-Clone.git
+cd torchlight-clone
 git config --global user.name "Cody Wilcox"
 git config --global user.email "cdwilcox1999@gmail.com"
 
+cmd /K
 PAUSE
